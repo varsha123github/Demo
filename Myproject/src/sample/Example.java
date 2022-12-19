b84debec070c12a9fa70fc42777c5b4cdc71d77a
varsha123github <varshadhokrat123@gmail.com>
2022-12-20 00:55:06 +0530

    added element in array

--- Myproject/src/sample/Example.java
+++ Myproject/src/sample/Example.java
@@ -1,9 +1,13 @@
 package sample;
 
+import java.util.*;
+
 public class Example {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		ArrayList a1=new ArrayList();
+		a1.add(10);
 
 	}
 
